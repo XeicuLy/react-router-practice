defcfbb920a5f031a5df8e5b441a63d62c024810
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2022-11-27 22:00:04 +0900

    :sparkles: feat: stateを渡す記述を追記

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,11 +1,14 @@
 import { Link, Outlet } from 'react-router-dom';
 
 export const Page1 = () => {
+  const arr = [...Array(100).keys()];
   return (
     <>
       <div>
         <p>Page1ページです</p>
-        <Link to='detailA'>Detail A</Link>
+        <Link to='detailA' state={arr}>
+          Detail A
+        </Link>
         <br />
         <Link to='detailB'>Detail B</Link>
       </div>
