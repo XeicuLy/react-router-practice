7062649ba3082646180582415288676e297940b7
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2022-11-27 16:08:11 +0900

    :sparkles: feat: ルーティング機能追加
    ページ遷移の基本を追記しました。

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
+
+import { Home } from './Home';
+import { Page1 } from './Page1';
+import { Page2 } from './Page2';
+
+export const App = () => {
+  return (
+    <BrowserRouter>
+      <div className='App'>
+        <Link to='/'>Home</Link>
+        <br />
+        <Link to='page1'>page1</Link>
+        <br />
+        <Link to='page2'>Page2</Link>
+      </div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/page1' element={<Page1 />} />
+        <Route path='/page2' element={<Page2 />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
